17670a346a58fc27eefd767e4d96a24f15890db9
Kevin Tong <kevin_tong100@hotmail.com>
2017-02-08 01:39:24 -0800

    added diceroll, coinflip, and poorly formatted 8ball

--- bot/index.js
+++ bot/index.js
@@ -58,6 +58,32 @@ module.exports = (TOKEN) => {
       const number = Math.floor(Math.random() * 100) + 1;
       return message.channel.sendMessage(`${message.author} rolled a ${number}`);
     }
+
+    if (message.content.startsWith(`${prefix}diceroll`)) {
+      const number = Math.floor(Math.random() * 6) + 1;
+      return message.channel.sendMessage(`${message.author} rolled a ${number}`);
+    }    
+
+    if (message.content.startsWith(`${prefix}coinflip`)) {
+      const coin = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+      return message.channel.sendMessage(`${message.author} flipped a ${coin}`);
+    }
+
+    if (message.content.startsWith(`${prefix}8ball`)) {
+      var responses = ['It is certain.', 'It is decidedly so', 'Without a doubt.',
+                        'Yes, definitely.', 'You may rely on it.', 'As I see it, yes.',
+                        'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.',
+                        'Reply hazy try again.', 'Ask again later.', 'Better not tell you now.',
+                        'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.',
+                        'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful'];
+      const question = message.content.substr(message.content.indexOf(" ") + 1);
+      if (question === "!8ball") {
+        return message.channel.sendMessage(`Ask the Magic 8 Ball a question ${message.author}`);
+      }
+      const response = responses[Math.floor(Math.random() * responses.length)];
+      message.channel.sendMessage(`${message.author} asked the Magic 8 Ball: ${question}`);
+      return message.channel.sendMessage(`The Magic 8 Ball says: ${response}`);
+    }
     
     return replyIfSticker(message);
   });
